2b38990b6c7b89c7e0f81d9588ccc5c93c64123a
nandhkumar77 <nandhkumargs8877@gmail.com>
2025-08-12 02:03:08 +0530

    final changes

--- app.py
+++ app.py
@@ -1,42 +1,36 @@
-import os 
+import os
 import sys
-
 import certifi
-ca=certifi.where()
-
+import pandas as pd
 from dotenv import load_dotenv
-load_dotenv()
+from fastapi import FastAPI, File, UploadFile, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from uvicorn import run as app_run
 
-mongo_db_url=os.getenv("MONGODB_URL_KEY")
-print(mongo_db_url)
-
-import pymongo
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.pipeline.training_pipeline import TrainingPipeline
-
-
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI,File,UploadFile,Request
-from uvicorn import run as app_run
-from fastapi.responses import  Response
-from starlette.responses import RedirectResponse
-import pandas as pd 
-from networksecurity.utils.ml_utils.model.estimator import NetworkModel
-
-
 from networksecurity.utils.main_utils.utils import load_object
-client=pymongo.MongoClient(mongo_db_url,tlsCAFILE=ca)
-from networksecurity.constants.training_pipeline import DATA_INGESTION_DATABASE_NAME
-from networksecurity.constants.training_pipeline import DATA_INGESTION_COLLECTION_NAME
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+from networksecurity.constants.training_pipeline import DATA_INGESTION_DATABASE_NAME, DATA_INGESTION_COLLECTION_NAME
 
-database=client[DATA_INGESTION_DATABASE_NAME]
-collection=database[DATA_INGESTION_COLLECTION_NAME]
+# Load environment variables
+load_dotenv()
+mongo_db_url = os.getenv("MONGODB_URL_KEY")
+ca = certifi.where()
 
-app=FastAPI()
-origins=["*"]
+# Initialize MongoDB client
+try:
+    client = pymongo.MongoClient(mongo_db_url, tlsCAFILE=ca)
+    database = client[DATA_INGESTION_DATABASE_NAME]
+    collection = database[DATA_INGESTION_COLLECTION_NAME]
+except Exception as e:
+    logging.error(f"Error connecting to MongoDB: {e}")
+    raise NetworkSecurityException(e, sys)
 
-
+app = FastAPI()
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -46,51 +40,40 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-from fastapi.templating import Jinja2Templates
-templates=Jinja2Templates(directory="./templates")
+templates = Jinja2Templates(directory="./templates")
 
-
-
-@app.get("/",tags=["authetication"])
+@app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
-
 
 @app.get("/train")
 async def train_route():
     try:
-        training_pipeline=TrainingPipeline()
-        training_pipeline.run_pipeline()
-        return Response("Training is successful")
+        # The training pipeline should not be run from the web server.
+        # This route should be removed, but for now, we will return an error.
+        return Response("Training is not supported via the web server. Please run the training pipeline locally or in a separate job.")
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
     
 @app.post("/predict")
-async def predict_route(request:Request,file:UploadFile=File(...)):
+async def predict_route(request: Request, file: UploadFile = File(...)):
     try:
-        df=pd.read_csv(file.file)
-        #print(df)
-        preprocessor=load_object("final_models/preprocessor.pkl")
-        final_model=load_object("final_models/model.pkl")
-        network_model=NetworkModel(preprocessor=preprocessor,model=final_model)
-
-        print(df.iloc[0])
-        Y_pred=network_model.predict(df)
-        print(Y_pred)
-        df["predicted_column"]=Y_pred
-        print(df["predicted_column"])
-        # df["predicted_column"].replace(-1,0)
-        #return df.to_json()
+        df = pd.read_csv(file.file)
+        
+        # Load the pre-trained model and preprocessor
+        preprocessor = load_object("final_models/preprocessor.pkl")
+        final_model = load_object("final_models/model.pkl")
+        
+        network_model = NetworkModel(preprocessor=preprocessor, model=final_model)
+        Y_pred = network_model.predict(df)
+        
+        df["predicted_column"] = Y_pred
         df.to_csv("prediction_output/output.csv")
-        table_html=df.to_html(classes="table table-striped")
-        #priint(table_html)
-        return templates.TemplateResponse("table.html",{"request":request,"table":table_html})
+        
+        table_html = df.to_html(classes="table table-striped")
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
-
+        raise NetworkSecurityException(e, sys)
 import uvicorn
-
-if __name__=="__main__":
-    #app_run(app,host="0.0.0.0",port=8080)
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
-## app_run indireclty runs the uvicorn app:app --reload
--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,6 +1,8 @@
-import os 
+import os
 import sys
-
+import dagshub
+import mlflow
+# ... other imports from your file ...
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging 
@@ -12,7 +14,6 @@ from networksecurity.utils.main_utils.utils import save_object,load_object
 from networksecurity.utils.main_utils.utils import load_numpy_array_data,evaluate_models
 from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
 
-import mlflow
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsClassifier
@@ -21,8 +22,15 @@ from sklearn.ensemble import (
     AdaBoostClassifier,RandomForestClassifier,GradientBoostingClassifier
 )
 
-import dagshub
-dagshub.init(repo_owner='GSNandakumar77', repo_name='networksecurity', mlflow=True)
+# This is the key change. We check if the token exists before running init.
+# For local dev, this will still run the interactive init.
+# For a container, it will use the token and bypass the interactive flow.
+if 'DAGSHUB_TOKEN' in os.environ:
+    os.environ['MLFLOW_TRACKING_URI'] = 'https://dagshub.com/GSNandakumar77/networksecurity.mlflow'
+    os.environ['MLFLOW_TRACKING_USERNAME'] = 'GSNandakumar77'
+    os.environ['MLFLOW_TRACKING_PASSWORD'] = os.environ['DAGSHUB_TOKEN']
+else:
+    dagshub.init(repo_owner='GSNandakumar77', repo_name='networksecurity', mlflow=True)
 
 
 class ModelTrainer:
@@ -39,16 +47,13 @@ class ModelTrainer:
             mlflow.log_metric("precision_score", classification_metric.precision_score)
             mlflow.log_metric("recall_score", classification_metric.recall_score)
 
-            if log_model_flag:  # save only if True
+            if log_model_flag:   # save only if True
                 model_path = "best_model"
                 if os.path.exists(model_path):
                     import shutil
                     shutil.rmtree(model_path)
                 mlflow.sklearn.save_model(best_model, model_path)
                 mlflow.log_artifacts(model_path, artifact_path="model")
-
-
-
 
     def train_model(self,X_train,Y_train,X_test,Y_test):
         models={
@@ -61,21 +66,13 @@ class ModelTrainer:
         params={
             "Decision Tree": {
                 'criterion':['gini', 'entropy', 'log_loss'],
-                # 'splitter':['best','random'],
-                # 'max_features':['sqrt','log2'],
             },
             "Random Forest":{
-                # 'criterion':['gini', 'entropy', 'log_loss'],
-                
-                # 'max_features':['sqrt','log2',None],
                 'n_estimators': [8,16,32,128,256]
             },
             "Gradient Boosting":{
-                # 'loss':['log_loss', 'exponential'],
                 'learning_rate':[.1,.01,.05,.001],
                 'subsample':[0.6,0.7,0.85,0.9],
-                # 'criterion':['squared_error', 'friedman_mse'],
-                # 'max_features':['auto','sqrt','log2'],
                 'n_estimators': [8,16,32,64,128,256]
             },
             "Logistic Regression":{},
@@ -86,53 +83,35 @@ class ModelTrainer:
             
         }
         model_report:dict=evaluate_models(X_train,Y_train,X_test,Y_test,
-                                          models=models,params=params)
-        ## to get best model score from dict 
+                                            models=models,params=params)
         best_model_score=max(sorted(model_report.values()))
-
-
-
-        ## to get the best model name 
         best_model_name=list(model_report.keys())[
             list(model_report.values()).index(best_model_score)]
         
-
         best_model=models[best_model_name]
         Y_train_pred=best_model.predict(X_train)
         Y_test_pred=best_model.predict(X_test)
 
         classification_train_metric=get_classification_score(Y_true=Y_train,Y_pred=Y_train_pred)
         
-
-        ##track the experiment end to end level with  mlflow -->
         self.track_mlflow(best_model,classification_train_metric)
-
-
-
 
         Y_test_pred=best_model.predict(X_test)
         classification_test_metric=get_classification_score(Y_true=Y_test,Y_pred=Y_test_pred)
 
-
-
         self.track_mlflow(best_model,classification_test_metric)
-
 
         preprocessor=load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
         model_dir_path=os.path.dirname(self.model_trainer_config.trained_model_file_path)
         os.makedirs(model_dir_path,exist_ok=True)
 
-
-
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(file_path=self.model_trainer_config.trained_model_file_path,obj=Network_Model)
-        ## save the model_object in seperate folder 
         save_object("final_models/model.pkl",best_model)
-        ##model trainer artifacts 
-
+        
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
-                            train_metric_artifact= classification_train_metric,
-                            test_metric_artifact=classification_test_metric)
+                                train_metric_artifact= classification_train_metric,
+                                test_metric_artifact=classification_test_metric)
         
         logging.info(f"Model trainer artifacts:{model_trainer_artifact}")
         return model_trainer_artifact
@@ -143,19 +122,15 @@ class ModelTrainer:
             train_file_path=self.data_transformation_artifact.transformed_train_file_path
             test_file_path=self.data_transformation_artifact.transformed_test_file_path
 
-
-            #loading training array and testing array 
             train_arr=load_numpy_array_data(train_file_path)
             test_arr=load_numpy_array_data(test_file_path)
 
-            
             X_train,Y_train,X_test,Y_test=(
                 train_arr[:,:-1],
                 train_arr[:,-1],
                 test_arr[:,:-1],
                 test_arr[:,-1]
             )
-
 
             model=self.train_model(X_train,Y_train,X_test,Y_test)
             
